830a9ee9b96f339d85928ef257af622151cf7841
Nguyen Dang Thanh <thanhnd4869@gmail.com>
2024-10-04 02:32:52 +0700

    create layout

---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Preloader.css';
+
+const Preloader = () => {
+    return (
+        <div className="st-preloader st-flex-center">
+            <div className="st-preloader-in" />
+        </div>
+    );
+};
+
+export default Preloader;
--- src/router/RouterLinks.jsx
+++ src/router/RouterLinks.jsx
@@ -1,15 +1,17 @@
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Layout from '../components/Layout/Layout';
 
 const RouterLinks = () => {
     return (
         <>
             <BrowserRouter>
                 <Routes>
-                    {/* <Route path="/" element={<Layout />}> */}
-                    {/* <Route index element={<Home />} /> */}
-                    {/* <Route path="*" element={<Page404 />} /> */}
-                    {/* <Route path="blog/blog-details" element={<BlogDetails />} /> */}
-                    {/* </Route> */}
+                    <Route path="/" element={<Layout />}>
+                        {/* <Route index element={<Home />} /> */}
+                        {/* <Route path="*" element={<Page404 />} /> */}
+                        {/* <Route path="blog/blog-details" element={<BlogDetails />} /> */}
+                    </Route>
                 </Routes>
             </BrowserRouter>
         </>
