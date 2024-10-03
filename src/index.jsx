fa6796df2e036bead00531af24dfcfe62152f4af
Nguyen Dang Thanh <thanhnd4869@gmail.com>
2024-10-04 03:34:12 +0700

    create home page

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import './styles/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/router/RouterLinks.jsx
+++ src/router/RouterLinks.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Layout from '../components/Layout/Layout';
+import Home from '../pages/Home/Home';
 
 const RouterLinks = () => {
     return (
@@ -8,7 +9,7 @@ const RouterLinks = () => {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Layout />}>
-                        {/* <Route index element={<Home />} /> */}
+                        <Route index element={<Home />} />
                         {/* <Route path="*" element={<Page404 />} /> */}
                         {/* <Route path="blog/blog-details" element={<BlogDetails />} /> */}
                     </Route>
