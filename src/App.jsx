1d9162ce41e60a7d368c8cb5bec1db0e75cc9ecd
Nguyen Dang Thanh <thanhnd4869@gmail.com>
2024-10-04 02:03:41 +0700

    create data & router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { useEffect } from 'react';
 import Aos from 'aos';
 
-import RouterLinks from './router';
+import RouterLinks from './router/RouterLinks';
 
-const App = () => {
+import 'aos/dist/aos.css';
+
+function App() {
     useEffect(() => {
         Aos.init({ once: true });
     }, []);
 
     return (
         <>
-            <RouterLinks></RouterLinks>
+            <RouterLinks />
         </>
     );
-};
+}
 
 export default App;
--- src/router/RouterLinks.jsx
+++ src/router/RouterLinks.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes } from 'react-router-dom';
 
 const RouterLinks = () => {
     return (
@@ -7,8 +7,6 @@ const RouterLinks = () => {
                 <Routes>
                     {/* <Route path="/" element={<Layout />}> */}
                     {/* <Route index element={<Home />} /> */}
-                    {/* <Route path="home-v3" element={<Home3 />} /> */}
-                    {/* <Route path="home-v4" element={<Home4 />} /> */}
                     {/* <Route path="*" element={<Page404 />} /> */}
                     {/* <Route path="blog/blog-details" element={<BlogDetails />} /> */}
                     {/* </Route> */}
